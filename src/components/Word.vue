eb6f32ad443d9bab77be6ad44389044b097b8207
S.Yano <b1013061@gmail.com>
2019-03-20 18:26:46 +0900

    Added Component Word

---
+++ src/components/Word.vue
@@ -0,0 +1,16 @@
+<template>
+  <span><input type="button" :value="word.message" @click="$emit('hoge',word.message)"></span>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  props: {
+    word: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
