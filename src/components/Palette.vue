8aafeef413bedfa7ed9117a9637fb82ce9a1eefd
S.Yano <b1013061@gmail.com>
2019-03-20 18:26:13 +0900

    Added Palette.vue

---
+++ src/components/Palette.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="palette">
+    <Word v-for="w in messages" :key="w" :word="w" v-on:hoge="pushWord"></Word>
+  </div>
+</template>
+
+<script>
+import Word from './Word.vue'
+
+export default {
+  name: 'app',
+  components: {
+      Word
+  },
+  data: ()=>{return {
+    messages: [
+    {message:'私',key:1},
+    {message:'は',key:2},
+    {message:'お腹が',key:3},
+    {message:'空いた',key:4}]
+  };},
+  methods: {
+    pushWord(msg) {
+      this.$emit('push',msg)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
